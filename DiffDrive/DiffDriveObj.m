81a5124f8928f4b464f531bcb659ba25517a5a61
spbisc97 <rotondi97simone@gmail.com>
2023-07-24 16:44:20 +0200

    Need to set the right controls

---
+++ DiffDrive/DiffDriveObj.m
@@ -0,0 +1,204 @@
+classdef DiffDriveObj < handle
+    properties (Access = public)
+        actual_x
+        actual_y
+        
+        delta_x
+        delta_y
+        
+        x
+        y
+        theta
+        
+        vartheta_x
+        vartheta_y
+        vartheta_theta
+        
+        vartheta_dot_x
+        vartheta_dot_y
+        vartheta_dot_theta
+        
+        vartheta_ddot_x
+        vartheta_ddot_y
+        vartheta_ddot_theta
+        
+        index
+        connections=zeros(1,128);
+    end
+    
+    properties (Access = private)
+        dot_x
+        dot_y
+        dot_theta
+        
+        P=0.001;
+        D=1;
+        K=0.1;
+    end
+    
+    methods (Access = public)
+        function obj = DiffDriveObj(index)
+            obj.x = 0;
+            obj.dot_x = 0;
+            obj.y = 0;
+            obj.dot_y = 0;
+            obj.theta = 0;
+            
+            obj.actual_x = 0;
+            obj.actual_y = 0;
+            
+            obj.vartheta_x=0;
+            obj.vartheta_y=0;
+            obj.vartheta_theta=0;
+            obj.vartheta_dot_x=0;
+            obj.vartheta_dot_y=0;
+            obj.vartheta_dot_theta=0;
+            obj.vartheta_ddot_x=0;
+            obj.vartheta_ddot_y=0;
+            obj.vartheta_ddot_theta=0;
+            obj.index = index;
+        end
+        
+        function obj = reset(obj)
+            obj.x = 0;
+            obj.dot_x = 0;
+            obj.y = 0;
+            obj.dot_y = 0;
+            obj.theta = 0;
+            obj.dot_theta = 0;
+            obj.vartheta_x=0;
+            obj.vartheta_y=0;
+            obj.vartheta_theta=0;
+            obj.vartheta_dot_x=0;
+            obj.vartheta_dot_y=0;
+            obj.vartheta_dot_theta=0;
+            obj.vartheta_ddot_x=0;
+            obj.vartheta_ddot_y=0;
+            obj.vartheta_ddot_theta=0;
+        end
+        
+        function obj = update(obj, u, dt, robots)
+            
+            summs=get_summs(obj,robots);
+            u_x=u(1);
+            u_y=u(2);
+            u_theta=u(3);
+            
+            obj.x = obj.x +cos(obj.theta) *obj.dot_x * dt;
+            obj.dot_x = obj.dot_x + cos(obj.theta)* u_x*dt ;
+            obj.y = obj.y + sin(obj.theta) *obj.dot_y * dt;
+            obj.dot_y = obj.dot_y +sin(obj.theta)* u_y *dt;
+            obj.theta = obj.theta + obj.dot_theta * dt;
+            obj.dot_theta = obj.dot_theta + u_theta * dt;
+            
+            obj.actual_x = obj.x + obj.delta_x;
+            obj.actual_y = obj.y + obj.delta_y;
+            
+            obj.vartheta_x=obj.vartheta_x+obj.vartheta_dot_x*dt;
+            obj.vartheta_y=obj.vartheta_y+obj.vartheta_dot_y*dt;
+            obj.vartheta_dot_x=obj.vartheta_dot_x+obj.vartheta_ddot_x*dt;
+            obj.vartheta_dot_y=obj.vartheta_dot_y+obj.vartheta_ddot_y*dt;
+            
+            obj.vartheta_ddot_x=-obj.P*summs(1)-obj.D*obj.vartheta_dot_x-u_x*dt;
+            obj.vartheta_ddot_y=-obj.P*summs(2)-obj.D*obj.vartheta_dot_y-u_y*dt;
+            
+            
+            obj.vartheta_theta=obj.vartheta_theta+obj.vartheta_dot_theta*dt;
+            obj.vartheta_dot_theta=obj.vartheta_dot_theta+obj.vartheta_ddot_theta*dt;
+            obj.vartheta_ddot_theta=-obj.P*summs(3)-obj.D*obj.vartheta_dot_theta-u_theta;
+            
+        end
+        
+        
+        function obj=set_connections(obj,connections)
+            for i=connections
+                if i~=0 && mod(i,1) == 0
+                    obj.connections(i)=true;
+                end
+            end
+        end
+        
+        function obj=remove_connections(obj,connections)
+            for i=connections
+                if mod(i,1) == 0
+                    obj.connections(i)=false;
+                end
+            end
+        end
+        
+        
+        function obj = set_state(obj, state)
+            obj.actual_x = state(1);
+            obj.x = state(1)-obj.delta_x;
+            obj.dot_x = state(2);
+            obj.actual_y = state(3);
+            obj.y = state(3)-obj.delta_y;
+            obj.dot_y = state(4);
+            obj.theta = state(5);
+            obj.dot_theta = state(6);
+            set_vartheta(obj,[obj.x;obj.y;obj.theta]);
+        end
+        
+        function obj = set_delta(obj, delta)
+            obj.delta_x = delta(1);
+            obj.delta_y = delta(2);
+        end
+        
+        function obj = set_xy(obj, p)
+            obj.x = p(1);
+            obj.y = p(2);
+        end
+        
+        function obj = set_dot_x(obj, v)
+            obj.dot_x = v(1);
+            obj.dot_y = v(2);
+        end
+        
+        function state = get_state(obj)
+            state(1) = obj.x;
+            state(2) = obj.dot_x;
+            state(3) = obj.y;
+            state(4) = obj.dot_y;
+        end
+        
+        function state = get_actual_state(obj)
+            state(1) = obj.actual_x;
+            state(2) = obj.dot_x;
+            state(3) = obj.actual_y;
+            state(4) = obj.dot_y;
+            
+        end
+        
+        function controls = get_controls(obj)
+            controls = -obj.K*[...
+                obj.x-obj.vartheta_x,...
+                obj.y-obj.vartheta_y,...
+                obj.theta-obj.vartheta_theta+randn()*(obj.x-obj.vartheta_x*0.1+obj.y-obj.vartheta_y*0.1)...
+                ];
+        end
+    end
+    methods (Access = private)
+        function summs= get_summs(obj,robots)
+            k=length(robots);
+            summs_x = 0;
+            summs_y = 0;
+            summs_theta = 0;
+            for i = 1:k
+                if class(robots(i)) ~= class(obj)
+                    error("The robot is not a PlaneDoubleIntObj");
+                end
+                if obj.connections(robots(i).index)
+                    summs_x =summs_x + obj.vartheta_x - robots(i).vartheta_x ;
+                    summs_y =summs_y + obj.vartheta_y - robots(i).vartheta_y ;
+                    summs_theta =summs_theta+ obj.vartheta_theta - robots(i).vartheta_theta ;
+                end
+            end
+            summs=[summs_x;summs_y;summs_theta];
+        end
+        function set_vartheta(obj, vartheta)
+            obj.vartheta_x=vartheta(1);
+            obj.vartheta_y=vartheta(2);
+            obj.vartheta_theta=vartheta(3);
+        end
+    end
+end
---
+++ DiffDrive/TestBase.m
@@ -0,0 +1,140 @@
+close all
+
+dt=0.25;
+tspan=0;
+Y=[];
+robots=[];
+n_robots=10;
+prev=n_robots;
+
+%% plot Settings
+colors=colormap();
+colors=colors(1:floor(end/n_robots):floor(end/n_robots)*n_robots,:);
+colors=colors(1:n_robots,:);%add this to each figure
+lw=0.2;%linewidth
+ls='--';%linestyle
+mk='o';%marker
+mi=500;%marker interval
+
+%% create adjacency matrix
+adj_mat=zeros(n_robots,n_robots);
+base_adj_line=zeros(1,n_robots);
+base_adj_line(1,2)=1;base_adj_line(1,end)=1;
+for index = 1:n_robots
+    adj_mat(index,:)=circshift(base_adj_line,[0,index-1]);
+end
+
+%% create delta matrix
+distance=3;
+delta_mat=zeros(n_robots,2);
+type=2;
+if type==1
+    for i=1:n_robots
+        delta_mat(i,1)=distance*cos(2*pi*i/n_robots);
+        delta_mat(i,2)=distance*sin(2*pi*i/n_robots);
+    end
+elseif type==2
+    angle=8*pi;
+    for i=1:n_robots
+        delta_mat(i,1)=distance*(i/n_robots)*cos(angle*i/n_robots);
+        delta_mat(i,2)=distance*(i/n_robots)*sin(angle*i/n_robots);
+    end
+end
+
+
+%% init robots
+for index = 1:n_robots
+    robot=DiffDriveObj(index);
+    robot.set_delta(delta_mat(index,:));
+    robot.set_connections(adj_mat(index,:).*[1:n_robots]);
+    robots=[robots,robot]; %#ok
+    prev=index;
+end
+%% set initial robot position
+for index = 1:n_robots
+    robots(index).set_state(randn(1,6).*[1,0,1,0,1,0]);
+    robots(index).update([0;0;0],dt,robots);
+    
+end
+y=[];
+for index = 1:n_robots
+    y=[y,robots(index).get_actual_state()];
+end
+Y=[Y;y];
+%% show adj matrix from robots
+for i=robots
+    disp(i.connections(1:n_robots));
+end
+
+%% run simulation
+t=0;
+while t<5e3
+    t=t+dt;
+    y=[];
+    for index = 1:n_robots
+        u=robots(index).get_controls();
+        robots(index).update(u,dt,robots);
+        y=[y,robots(index).get_actual_state()];
+    end
+    Y=[Y;y];
+    tspan=[tspan,t-dt];
+    if t>100 && sum(abs(Y(end,:)-Y(end-1,:)))<1e-6
+        %break condition
+        break
+    end
+    
+end
+%% plots
+%time-position plane
+figure(1)
+colororder(colors())
+nexttile
+plot(tspan,Y(:,1:4:end),...
+    'LineWidth',lw, ...
+    'LineStyle',ls,...
+    'Marker',mk,...
+    'MarkerIndices',1:mi:length(tspan))
+legend()
+nexttile
+plot(tspan,Y(:,3:4:end), ...
+    'LineWidth',lw, ...
+    'LineStyle',ls,...
+    'Marker',mk,...
+    'MarkerIndices',1:mi:length(tspan))
+legend
+
+%x-y plane
+figure(2)
+colororder(colors())
+
+plot(Y(:,1:4:end),Y(:,3:4:end), ...
+    'LineWidth',lw, ...
+    'LineStyle',ls,...
+    'Marker',mk,...
+    'MarkerIndices',1:mi:length(tspan))
+axis square
+axis equal
+hold on
+z_c=[];
+for index = 1:n_robots
+    z_c=[z_c,robots(index).get_state()];
+end
+C=[sum(z_c(:,1:4:end))/n_robots,sum(z_c(:,3:4:end))/n_robots];
+disp([(((1:n_robots).')),(z_c(:,1:4:end).'),(z_c(:,3:4:end).')])
+last_t=tspan(end);
+if type==1
+    plot(cos(tspan/last_t)+C(1),sin(tspan/last_t)+C(2), ...
+        'LineWidth',0.1 )
+    axis square
+    axis equal
+    
+elseif type==2
+    plot(distance.*(tspan/last_t).*cos(angle*tspan/last_t)+C(1),...
+        distance.*(tspan/last_t).*sin(angle*tspan/last_t)+C(2),...
+        'LineWidth',0.1)
+    axis square
+    axis equal
+    
+end
+
+legend
